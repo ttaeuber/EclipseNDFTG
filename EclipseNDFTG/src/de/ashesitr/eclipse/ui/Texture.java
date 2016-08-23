91c00998ccf1bd3c47f11252e8e7312b037e2c29
Theresa_2 <Theresa_2@Theresa-PC.fritz.box>
2016-08-23 20:37:36 +0200

    Mipmapping fix für OpenGL < 3.0

--- EclipseNDFTG/src/de/ashesitr/eclipse/ui/Texture.java
+++ EclipseNDFTG/src/de/ashesitr/eclipse/ui/Texture.java
@@ -2,32 +2,48 @@ package de.ashesitr.eclipse.ui;
 
 import java.nio.ByteBuffer;
 
+import org.lwjgl.opengl.GL;
+
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL13.*;
+import static org.lwjgl.opengl.GL14.*;
 import static org.lwjgl.opengl.GL30.*;
 
 public class Texture {
 	private final int textureID, textureUnit;
-	
+
 	public Texture(int width, int height, ByteBuffer pixelData, int textureUnit) {
 		this.textureUnit = textureUnit;
 		this.textureID = glGenTextures();
 		glActiveTexture(textureUnit);
 		glBindTexture(GL_TEXTURE_2D, textureID);
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelData);
-		glGenerateMipmap(GL_TEXTURE_2D);
+		
+		if (GL.getCapabilities().OpenGL30) {
+			// GL 3.0 Mipmapping
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelData);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		} else {
+			// GL 1.4+ Mipmapping
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR); 
+			glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_TRUE);
+			
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelData);
+		}
 	}
-	
+
 	public void bind() {
 		glActiveTexture(textureUnit);
 		glBindTexture(GL_TEXTURE_2D, textureID);
 	}
-	
+
 	public void unbind() {
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
-	
+
 	public void destroy() {
 		glDeleteTextures(textureID);
 	}
